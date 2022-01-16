20718ae76965f551304d35a6531aed37ad7e49a1
ggkguelensan <gkguelensan@gmail.com>
2022-01-16 16:56:19 +0300

    f die error handler

--- kilo.c
+++ kilo.c
@@ -6,6 +6,12 @@
 
 struct termios orig_termios;
 
+void die(char const *err_msg)
+{
+    perror(err_msg);
+    exit(EXIT_FAILURE);
+}
+
 void disableRawMode()
 {
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
@@ -23,6 +29,8 @@ void enableRawMode()
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag &= ~(CS8);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -31,9 +39,11 @@ int main(void)
 {
     enableRawMode();
 
-    char c;
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    while(1)
     {
+        char c = '\0';
+        ssize_t read_ret = read(STDIN_FILENO, &c, 1);
+        if(read_ret < 0) die("read");
         if(iscntrl(c))
         {
             printf("%d\r\n", c);
@@ -42,6 +52,7 @@ int main(void)
         {
             printf("%d ('%c')\r\n", c, c);
         }
+        if(c == 'q') break;
     };
     return EXIT_SUCCESS;
 }
